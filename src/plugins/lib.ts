e0fd8a307dd74423c2d5e65fc06d47fd3065540f
zhangyh <zhdhr0000@qq.com>
2021-04-02 18:31:10 +0800

    add infinite-scroll feature.

--- src/plugins/lib.ts
+++ src/plugins/lib.ts
@@ -8,8 +8,10 @@ import VeLine from "v-charts/lib/line.common";
 import VeBar from "v-charts/lib/bar.common";
 import VeCandle from "v-charts/lib/candle.common";
 import "echarts/lib/component/markLine";
+import infiniteScroll from "vue-infinite-scroll";
 
 dayjs.extend(relativeTime);
+Vue.use(infiniteScroll);
 Vue.use(UIKit);
 Vue.component(VeLine.name, VeLine);
 Vue.component(VeBar.name, VeBar);
