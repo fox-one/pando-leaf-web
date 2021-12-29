d6be79efb9501114276f0f2d19f691fbc8c6ea82
Lyric Wai <5h3ll3x@gmail.com>
2021-12-29 20:18:11 +0900

    add a ga (#10)

--- src/constants.ts
+++ src/constants.ts
@@ -5,7 +5,7 @@ export const VERSION = pkg.version;
 
 export const isProduct = process.env.APP_ENV === "prod";
 
-export const GA = "";
+export const GA = "UA-112996081-17";
 
 export const NODE_ENV = process.env.NODE_ENV;
 export const APP_TOKEN = process.env.TOKEN;
