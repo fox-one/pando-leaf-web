070f6cb031a647bc87ea99152de94c83fa671f6f
zhangyh <zhdhr0000@qq.com>
2022-10-27 11:35:02 +0800

    feat: update route middleware.

--- src/middleware/route.ts
+++ src/middleware/route.ts
@@ -1,9 +1,13 @@
 import type { Middleware } from "@nuxt/types";
 
-const auth: Middleware = function ({ route, redirect }) {
+const auth: Middleware = function ({ store, route, redirect }) {
   const current = route.name;
 
   if (!current || current === "index") {
+    if (store.getters["auth/isLogged"]) {
+      redirect({ name: "vault" });
+      return;
+    }
     redirect({ name: "market" });
     return;
   }
