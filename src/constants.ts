f582f447c95e956a932fc59baad2507477156560
zhangyh <zhdhr0000@qq.com>
2022-12-02 10:31:12 +0800

    update dev url base.

--- src/constants.ts
+++ src/constants.ts
@@ -25,7 +25,7 @@ export const CONFIG = isProduct
       MIXIN_CLIENT_ID: "75f18fe8-b056-46d6-9c48-0214425e58ce",
     }
   : {
-      API_BASE: "https://pando-test-api.fox.one/api/",
+      API_BASE: "https://leaf-api.aspens.rocks/api/",
       WS_BASE: "wss://",
       MIXIN_CLIENT_ID: "670e1faa-2975-48d9-a81f-cd0905ae847e",
     };
